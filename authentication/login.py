f65bf4cedbb5daefe0598c766312ff0fe663cfab
vmalepati1 <malepativikas@gmail.com>
2020-04-13 15:51:08 -0400

    Added bcrypt encryption for passwords

--- authentication/login.py
+++ authentication/login.py
@@ -1,5 +1,6 @@
 from authentication.user import User
 from database.db import Database
+import bcrypt
 
 # This class manages the operators's login procedure
 class Login():
@@ -11,9 +12,9 @@ class Login():
     def validate_user(self, username, password):
         # Find operator by credentials
         cur = self.db.query("""
-                            SELECT IS_admin, Action_permissions, Table_permissions FROM operators WHERE
-                            Username = %s AND Password = %s;""",
-                            [username, password])
+                            SELECT Password, IS_admin, Action_permissions, Table_permissions FROM operators 
+                            WHERE Username = %s""",
+                            [username])
 
         # Fetch one row with pertinent information
         r = cur.fetchone()
@@ -22,6 +23,12 @@ class Login():
         if not r:
             return {"status": "failed", "details": "Username/password incorrect"}
 
-        # Return the success code along with the populated User object            
-        return {"status": "success", "user": User(username, r[0], r[1], r[2])}
-        
+        # Get the password hash from database
+        db_hash = r[0].encode()
+
+        # Check that the hash and the given password match
+        if bcrypt.checkpw(password.encode(), db_hash):
+            # Return the success code along with the populated User object            
+            return {"status": "success", "user": User(username, r[1], r[2], r[3])}
+
+        return {"status": "failed", "details": "Username/password incorrect"}
--- main.py
+++ main.py
@@ -34,4 +34,4 @@ app.include_router(
 # default settings. Use by typing: python main.py
 if __name__ == "__main__":
   import uvicorn
-  uvicorn.run(app, host="0.0.0.0", port=8000)
+  uvicorn.run(app, host="127.0.0.1", port=8000)
